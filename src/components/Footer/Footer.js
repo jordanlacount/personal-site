8af62247100f23eb47fbee780901b110d045a8ee
Jordan LaCount <jordanlacount@Jordans-MacBook-Pro-2.local>
2021-07-19 19:38:28 -0600

    add footer component and story

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+export function Footer() {
+    return (
+        <footer className="footer-wrapper">
+            <ul>
+                <li>link</li>
+                <li>lank</li>
+                <li>lunk</li>
+            </ul>
+        </footer>
+    )
+}
