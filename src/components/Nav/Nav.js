cd2950f67ae1dff56807fa7c5407d1ac0c60ea1f
Jordan LaCount <jordanlacount@Jordans-MacBook-Pro-2.local>
2021-07-19 19:05:59 -0600

    initial commit

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Nav.css"
+
+export function Nav() {
+    return (
+        <nav className="nav-wrapper">
+            <h1>Some Text In It</h1>
+            <ul>
+                <li>a thing</li>
+                <li>another one</li>
+                <li>last one</li>
+            </ul>
+        </nav>
+    )
+}
---
+++ src/components/Nav/Nav.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { Nav } from './Nav'
+
+export default {
+    title: 'components/Nav',
+    component: Nav,
+}
+
+export const Primary = () => <Nav />
